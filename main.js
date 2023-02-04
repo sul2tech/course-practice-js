076811f023576930a42282a6a1cdb833db2769b4
Sul2tech <jonathawayrd@gmail.com>
2023-02-04 10:56:53 -0500

    carrito y desktopmobil ready

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('#product-detail-close');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
+
+
+
+
+
 
 function toggleDesktopMenu() {
   const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
@@ -16,6 +24,9 @@ function toggleDesktopMenu() {
   if (!isAsideClosed) {
     shoppingCartContainer.classList.add('inactive');
   }
+
+  
+
   
   desktopMenu.classList.toggle('inactive');
 }
@@ -26,6 +37,8 @@ function toggleMobileMenu() {
   if (!isAsideClosed) {
     shoppingCartContainer.classList.add('inactive'); 
   }
+
+  closeProductDetailAside()
   
   mobileMenu.classList.toggle('inactive');
 }
@@ -36,9 +49,40 @@ function toggleCarritoAside() {
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add('inactive'); 
   }
+
+
+  const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+  
+  if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive'); 
+  }
   
   shoppingCartContainer.classList.toggle('inactive');
+
+
+
+  
 }
+
+
+function openProductDetailAside() {
+
+
+  shoppingCartContainer.classList.add('inactive');
+
+  productDetailContainer.classList.remove('inactive');
+  
+  
+
+
+}
+
+function closeProductDetailAside() {
+  productDetailContainer.classList.add('inactive');
+  
+}
+
+
 
 const productList = [];
 productList.push({
@@ -65,6 +109,7 @@ function renderProducts(arr) {
     // product= {name, price, image} -> product.image
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside)
   
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
